72b2dd70f1da1d25e926d324d17c1a62d16c7b16
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2025-12-25 12:52:13 +0600

    fix: turbo streams and security bug

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,19 +4,19 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   # later_to makes broadcasting part asynchronous using background jobs
-  after_create_commit do
-    broadcast_prepend_later_to(
-      "#{company}-quotes",
-      partial: 'quotes/quote',
-      locals: { quote: self },
-      target: 'quotes'
-    )
-  end
-  after_update_commit -> { broadcast_replace_later_to "#{company}-quotes" }
-  after_destroy_commit lambda {
-    broadcast_remove_to "#{company}-quotes"
-  } # broadcast_remove_later_to method does not exist
+  # after_create_commit do
+  #   broadcast_prepend_later_to(
+  #     [ self.company, 'quotes' ],
+  #     partial: 'quotes/quote',
+  #     locals: { quote: self },
+  #     target: 'quotes'
+  #   )
+  # end
+  # after_update_commit -> { broadcast_replace_later_to [ self.company, 'quotes' ] }
+  # after_destroy_commit do
+  #   broadcast_remove_to [ self.company, 'quotes' ]
+  # end # broadcast_remove_later_to method does not exist
 
   # Above three callbacks are equivalent to the following single line
-  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [ quote.company, 'quotes' ] }, inserts_by: :prepend
 end
